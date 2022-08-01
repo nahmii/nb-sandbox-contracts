d37437e51959ecb3e00eda47ebec11594c61a5ef
menawar <sylvesternathan93@gmail.com>
2022-08-01 13:39:09 -0400

    added disperse contact and docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:16-alpine
+
+WORKDIR /usr/src/app
+RUN apk add --no-cache python3 make g++
+
+COPY package*.json ./
+RUN npm install
+
+COPY test/* test/
+COPY scripts/* scripts/
+COPY contracts/* contracts/
+COPY tsconfig.json .
+COPY hardhat.config.ts .
+
+RUN npm run compile
+RUN npm run test
+
+COPY $PWD/docker/* /usr/local/bin/
+ENTRYPOINT ["/bin/sh", "/usr/local/bin/entrypoint.sh"]
--- contracts/CBToken.sol
+++ contracts/CBToken.sol
@@ -9,9 +9,11 @@ contract CBToken is ERC20, AccessControl {
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     uint8 private _decimals;
 
-    constructor(string memory name_, string memory symbol_, uint8 decimals_)
-        ERC20(name_, symbol_)
-    {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) ERC20(name_, symbol_) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(BURNER_ROLE, msg.sender);
@@ -22,7 +24,7 @@ contract CBToken is ERC20, AccessControl {
     function decimals() public view virtual override returns (uint8) {
         return _decimals;
     }
-   
+
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
