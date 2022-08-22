a84fed1769d3246fe4688a1d3f098dfab7ea7370
menawar <sylvesternathan93@gmail.com>
2022-08-22 10:01:23 -0400

    added dynamic address input

--- scripts/token-swap.ts
+++ scripts/token-swap.ts
@@ -2,8 +2,8 @@ import { ethers } from "hardhat";
 
 async function main() {
   const TokenSwap = await ethers.getContractFactory("TokenSwap");
-  const cBTokenAddress = "0x455e7020Dad8cF6FD17C7Bcd8Aa97A86968d739D";
-  const cBSTokenAddress = "0x45b96521b9bB38c6C677Ae08A22aE5079D2aA992";
+  const cBTokenAddress = `${process.env.cBTokenAddress}`;
+  const cBSTokenAddress = `${process.env.cBSTokenAddress}`;
   const tokenSwap = await TokenSwap.deploy(cBTokenAddress, cBSTokenAddress);
 
   await tokenSwap.deployed();
