db9116260e294191fc6d368d54a726a3d5091240
Jens Ivar Jordre <jensivar@nahmii.io>
2022-08-23 20:22:16 +0200

    Replace custom Disperse contract by the original by banteg

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,18 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'hardhat-dependency-compiler'
+import 'solidity-coverage';
 
 dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -23,17 +24,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.16",
+  solidity: {
+    compilers: [
+      { version: '0.8.16' },
+      {version: '0.4.26' }
+    ]
+  },
   networks: {
     mainnet: {
-      chainId: parseInt(process.env.CHAIN_ID ?? "0"),
-      url: process.env.MAINNET_URL || "",
+      chainId: parseInt(process.env.CHAIN_ID ?? '0'),
+      url: process.env.MAINNET_URL || '',
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     testnet: {
-       chainId: parseInt(process.env.CHAIN_ID ?? "0"),
-      url: process.env.TESTNET_URL || "",
+      chainId: parseInt(process.env.CHAIN_ID ?? '0'),
+      url: process.env.TESTNET_URL || '',
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
@@ -41,6 +47,11 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
+  dependencyCompiler: {
+    paths: [
+      'token-research/contracts/Disperse.sol',
+    ],
+  }
 };
 
 export default config;
