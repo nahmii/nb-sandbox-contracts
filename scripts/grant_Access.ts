99b866ecfdacf7c0d762faf66efa9679d33ad9fd
menawar <sylvesternathan93@gmail.com>
2022-08-24 21:30:19 -0400

    added dynamic roles

--- scripts/grant_Access.ts
+++ scripts/grant_Access.ts
@@ -1,27 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const CBSTokenAddress = `${process.env.cBSTokenAddress}`;
-  const swapperAddress = `${process.env.swapperAddress}`;
+  const contractAddress = `${process.env.CONTRACT_ADDRESS}`;
+  const actorAddress = `${process.env.ACTOR_ADDRESS}`;
 
-  const CBSToken = await ethers.getContractAt("CBToken", CBSTokenAddress);
-  const minterRole =
-    "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6";
-  const burnerRole =
-    "0x3c11d16cbaffd01df69ce1c404f6340ee057498f5f00246190ea54220576a848";
-  const grantMinterRole = await CBSToken.grantRole(minterRole, swapperAddress);
-  const grantBurnerRole = await CBSToken.grantRole(burnerRole, swapperAddress);
-  await grantMinterRole.wait();
-  await grantBurnerRole.wait();
-
-  console.log(
-    "Swapper has minting role:",
-    await CBSToken.hasRole(minterRole, swapperAddress)
-  );
-  console.log(
-    "Swapper has burning role:",
-    await CBSToken.hasRole(burnerRole, swapperAddress)
-  );
+  const contract = await ethers.getContractAt("AccessControl", contractAddress);
+  const roles = `${process.env.ROLES}`.split(",");
+  for (let i = 0; i < roles.length; i++) {
+    const role = roles[i];
+    const grantRole = await contract.grantRole(role, actorAddress);
+    await grantRole.wait();
+    console.log(
+      `${actorAddress} has ${role} role:`,
+      await contract.hasRole(role, actorAddress)
+    );
+  }
 }
 
 main().catch((error) => {
