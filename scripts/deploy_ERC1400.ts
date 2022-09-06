6d1b978802dfe8cc3fd7e86f2bcb0f5cd0193f8c
menawar <sylvesternathan93@gmail.com>
2022-09-06 10:58:15 -0400

    added script to deploy ERC1400 token

---
+++ scripts/deploy_ERC1400.ts
@@ -0,0 +1,30 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const ERC1400 = await ethers.getContractFactory("ERC1400");
+  const controller = `${process.env.CONTROLLER}`;
+  const partition1 =
+    "0x7265736572766564000000000000000000000000000000000000000000000000"; // reserved in hex
+  const partition2 =
+    "0x6973737565640000000000000000000000000000000000000000000000000000"; // issued in hex
+  const partition3 =
+    "0x6c6f636b65640000000000000000000000000000000000000000000000000000"; // locked in hex
+  const partitions = [partition1, partition2, partition3];
+
+  const erc1400 = await ERC1400.deploy(
+    "CBSToken",
+    "CBST",
+    1,
+    [controller],
+    partitions
+  );
+
+  await erc1400.deployed();
+
+  console.log("Token deployed to:", erc1400.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
