0c0c0f0ad512d202fe2e0b77ff12f42608d5e079
Jens Ivar Jordre <jensivar@nahmii.io>
2022-10-03 16:00:35 +0200

    Update constructor parameters

--- scripts/deploy_CBSToken.ts
+++ scripts/deploy_CBSToken.ts
@@ -8,15 +8,13 @@ async function main() {
   const partition3 = `${process.env.PARTITION_3}`;
 
   const partitions = [partition1, partition2, partition3];
-  const chainID = `${process.env.CHAIN_ID}`;
 
   const cbsToken = await cBSToken.deploy(
     "CBSToken",
     "CBST",
     4,
     [controller],
-    partitions,
-    chainID
+    partitions
   );
 
   await cbsToken.deployed();
